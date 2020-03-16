0a22a3bc7e5df3a766406b161221fd1ffd805393
LuanAmaral <luan.rocha.amaral@gmail.com>
2020-03-16 17:50:33 -0300

    Inicio do programa para ler as setas do teclado.

--- scripts/gpar_hulk_manual.py
+++ scripts/gpar_hulk_manual.py
@@ -23,3 +23,33 @@ def Move():
     vel.angular.z = 0
 
     while not rospy.is_shutdown()
+        inkey = _Getch()
+        while(1):
+                k=inkey()
+                if k!='':break
+
+            if k=='\x1b[A':
+                    print "up"
+                    vel.linear.x  =  0.2;
+                    vel.angular.z =  0.0;
+
+            elif k=='\x1b[B':
+                    print "down"
+                    vel.linear.x  = -0.2;
+                    vel.angular.z =  0.0;
+
+            elif k=='\x1b[C':
+                    print "right"
+                    vel.linear.x  =  0.0;
+                    vel.angular.z =  0.2;
+
+            elif k=='\x1b[D':
+                    print "left"
+                    vel.linear.x  =  0.0;
+                    vel.angular.z = -0.2;
+            else:
+                    print "not an arrow key!"
+
+            velocity_publisher.publish(vel)
+            rospy.loginfo(vel)
+            
